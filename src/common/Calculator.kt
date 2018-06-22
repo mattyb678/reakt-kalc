c3ba6643afef35bc9fab0258db56f532edbf9c97
Matt Berteaux <mattyb678@gmail.com>
2018-06-21 19:18:23 -0600

    Artithmetic operations

--- src/common/Calculator.kt
+++ src/common/Calculator.kt
@@ -5,11 +5,16 @@ import app.KalcState
 class Calculator {
     companion object {
         fun calculate(calcState: KalcState, operation: Operation): KalcState {
-            if (operation is Num) {
+            if (operation is Eq) {
+                calcState.total = calcState.operation!!.operate(calcState.total ?: 0.0, calcState.next ?: 0.0)
+                calcState.next = null
+                calcState.operation = null
+                return calcState
+            } else if (operation is Num) {
                 calcState.next = (calcState.next ?: 0.0) * 10 + operation.num.toDouble()
                 return calcState
             } else if (operation is MultiStepOperation) {
-                calcState.total = calcState.next
+                calcState.total = calcState.next ?: calcState.total
                 calcState.next = 0.0
                 calcState.operation = operation
                 return calcState
--- src/common/Operations.kt
+++ src/common/Operations.kt
@@ -1,66 +1,106 @@
 package common
 
 interface Operation : Displayable {
-
+    fun operate(total: Double, next: Double) : Double
 }
 
 interface MultiStepOperation : Operation
 
 class Clear : Operation {
+    override fun operate(total: Double, next: Double): Double {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
     override fun getDisplay(): String {
         return "AC"
     }
 }
 
 class Negate : Operation {
+    override fun operate(total: Double, next: Double): Double {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
     override fun getDisplay(): String {
         return "+/-"
     }
 }
 
 class Percent : Operation {
+    override fun operate(total: Double, next: Double): Double {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
     override fun getDisplay(): String {
         return "%"
     }
 }
 
 class Divide : MultiStepOperation {
+    override fun operate(total: Double, next: Double): Double {
+        return total / next
+    }
+
     override fun getDisplay(): String {
         return "÷"
     }
 }
 
 class Multiply : MultiStepOperation {
+    override fun operate(total: Double, next: Double): Double {
+        return total * next
+    }
+
     override fun getDisplay(): String {
         return "x"
     }
 }
 
 class Add : MultiStepOperation {
+    override fun operate(total: Double, next: Double): Double {
+        return total + next
+    }
+
     override fun getDisplay(): String {
         return "+"
     }
 }
 
 class Subtract : MultiStepOperation {
+    override fun operate(total: Double, next: Double): Double {
+        return total - next
+    }
+
     override fun getDisplay(): String {
         return "-"
     }
 }
 
 class Eq : Operation {
+    override fun operate(total: Double, next: Double): Double {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
     override fun getDisplay(): String {
         return "="
     }
 }
 
 class Decimal : Operation {
+    override fun operate(total: Double, next: Double): Double {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
     override fun getDisplay(): String {
         return "."
     }
 }
 
 class Num(val num: Int) : Operation {
+    override fun operate(total: Double, next: Double): Double {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
     override fun getDisplay(): String {
         return "$num"
     }
